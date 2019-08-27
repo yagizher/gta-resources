3dbd73641f2a667694d71e905cc1613a58fd7780
DiscworldZA <pd.van.lill@gmail.com>
2019-08-27 13:07:03 +0200

    oops wrong place

--- disc-social/server/main.lua
+++ disc-social/server/main.lua
@@ -3,7 +3,8 @@ for k, v in pairs(Config.Socials) do
         local msg = 'Social: ' .. v.social .. ': ' .. v.response
         local fal = '^1SYSTEM'
         TriggerClientEvent('chat:addMessage', src, {
-            args = { fal, msg, template = v.template }
+            template = v.template,
+            args = { fal, msg }
         })
     end)
 end
