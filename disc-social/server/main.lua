ff03e7d6a722a9a58e4801884d4cd928b1f5ee5c
DiscworldZA <pd.van.lill@gmail.com>
2019-08-27 12:50:14 +0200

    fix messaging

--- disc-social/server/main.lua
+++ disc-social/server/main.lua
@@ -1,5 +1,9 @@
 for k, v in pairs(Config.Socials) do
     RegisterCommand(v.command, function(src, args, raw)
-        TriggerClientEvent('chat:addMessage', src, 'Social: ' .. v.social .. ': ' .. v.response)
+        local msg = 'Social: ' .. v.social .. ': ' .. v.response
+        local fal = '^1SYSTEM'
+        TriggerClientEvent('chat:addMessage', src, {
+            args = { fal, msg }
+        })
     end)
 end
