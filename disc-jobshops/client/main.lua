97a3a694073fd1633019f97866b754a9d2192d24
DiscworldZA <pd.van.lill@gmail.com>
2019-09-03 15:21:49 +0200

    add jobshops WIP

---
+++ disc-jobshops/client/main.lua
@@ -0,0 +1,71 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj)
+            ESX = obj
+        end)
+        Citizen.Wait(0)
+    end
+
+    while ESX.GetPlayerData().job == nil do
+        Citizen.Wait(10)
+    end
+
+    ESX.PlayerData = ESX.GetPlayerData()
+end)
+
+RegisterNetEvent('esx:setJob')
+AddEventHandler('esx:setJob', function(job)
+    ESX.PlayerData.job = job
+end)
+
+Citizen.CreateThread(function()
+    for _, v in pairs(Config.Shops) do
+        local marker = {
+            name = v.name,
+            type = v.markerType,
+            coords = v.coords,
+            colour = v.colour,
+            size = vector3(1.0, 1.0, 1.0),
+            msg = 'Press ~INPUT_CONTEXT~ to open the Shop',
+            action = function()
+                OpenShopMenu(v)
+            end,
+            shouldDraw = function()
+                return ESX.PlayerData.job.name == v.job
+            end
+        }
+        TriggerEvent('disc-base:registerMarker', marker)
+    end
+end)
+
+function OpenShopMenu(shop)
+    local options = {}
+
+    for _, v in shop.items do
+        table.insert(options, { label = v.label .. (' - <span style="color:green;">$%s</span>'):format(v.price), action = function()
+            BuyItem(v.item, v.price)
+        end })
+    end
+
+    local menu = {
+        title = shop.name,
+        name = 'shop_items',
+        options = options
+    }
+    TriggerEvent('disc-base:openMenu', menu)
+end
+
+function BuyItem(item, price)
+    ESX.TriggerServerCallback('disc-shops:buyItem', function(result)
+        if result == 1 then
+            exports['mythic_notify']:DoHudText('success', 'Item bought!')
+        elseif result == 0 then
+            exports['mythic_notify']:DoHudText('error', 'You need $' .. price)
+        else
+            exports['mythic_notify']:DoHudText('error', 'You do not have inventory space')
+        end
+    end
+    , item, price)
+end
---
+++ disc-jobshops/config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.Shops = {
+    {
+        name = 'Test Shop',
+        coords = vector(0, 0, 0),
+        colour = { r = 55, b = 255, g = 55 },
+        markerType = 1,
+        blip = false,
+        items = {
+            { item = 'bread', label = 'Bread', price = 500 }
+        },
+        job = 'mafia'
+    }
+}
