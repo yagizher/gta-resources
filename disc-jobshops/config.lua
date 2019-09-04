be09dfe5573880703c8a8a70f7170ccd57fd849c
DiscworldZA <pd.van.lill@gmail.com>
2019-09-04 20:09:27 +0200

    remove blip for now

--- disc-jobshops/config.lua
+++ disc-jobshops/config.lua
@@ -6,7 +6,6 @@ Config.Shops = {
         coords = vector3(-399.6, 58.5, 54.94),
         colour = { r = 55, b = 255, g = 55 },
         markerType = 1,
-        blip = false,
         items = {
             { item = 'bread', label = 'Bread', price = 500 }
         },
