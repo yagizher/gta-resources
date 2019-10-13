f7f0d52fede1e67de1337f55e7442bb255277862
DiscworldZA <pd.van.lill@gmail.com>
2019-10-14 00:45:13 +0200

    update shop config to be less confusing

--- disc-inventoryhud/config.lua
+++ disc-inventoryhud/config.lua
@@ -8,15 +8,7 @@ Config.Shops = {
         coords = vector3(44.38, -1746.76, 29.5),
         items = {
             { name = "bread", price = 100, count = 10 },
-            { name = "idcard", price = 1000, count = 1 },
-            { name = "energy", price = 1000, count = 1 },
-        }
-    },
-    ['My Shop Of Dark Things'] = {
-        coords = vector3(44.38, -1742.76, 29.5),
-        items = {
-            { name = "weed", price = 10000, count = 10 },
-            { name = "WEAPON_SNSPISTOL", price = 10000, count = 10 },
+            { name = "water", price = 100, count = 10 },
         }
     }
 }
