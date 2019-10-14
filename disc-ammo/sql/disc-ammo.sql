94e48a39a4b934aa5eedaf4294b439e235364213
DiscworldZA <pd.van.lill@gmail.com>
2019-10-14 23:46:49 +0200

    sql updates rip

--- disc-ammo/sql/disc-ammo.sql
+++ disc-ammo/sql/disc-ammo.sql
@@ -1,2 +1,16 @@
+create table disc_ammo
+(
+    id bigint unsigned auto_increment,
+    owner text not null,
+    hash text not null,
+    count int default 0 not null,
+    constraint id
+        unique (id)
+);
+
+alter table disc_ammo
+    add primary key (id);
+
+
 INSERT INTO essentialmode.items (name, label, `limit`, rare, can_remove) VALUES ('disc_ammo_pistol', 'Pistol Ammo', 10, 0, 1);
 INSERT INTO essentialmode.items (name, label, `limit`, rare, can_remove) VALUES ('disc_ammo_pistol_large', 'Pistol Ammo Large', 10, 0, 1);
