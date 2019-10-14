cdbfc85a18e9eefa660a1fc0d0b1a963b4ce3768
DiscworldZA <pd.van.lill@gmail.com>
2019-10-14 02:05:05 +0200

    add ammo mod

---
+++ disc-ammo/config.lua
@@ -0,0 +1,22 @@
+Config = {}
+Config.EnableInventoryHUD = true
+Config.ReloadTime = 2000 --ms
+
+Config.Ammo = {
+    {
+        name = 'disc_ammo_pistol',
+        weapons = {
+            `WEAPON_PISTOL`,
+            `WEAPON_SNSPISTOL`
+        },
+        count = 30
+    },
+    {
+            name = 'disc_ammo_pistol_large',
+            weapons = {
+                `WEAPON_PISTOL`,
+                `WEAPON_SNSPISTOL`
+            },
+            count = 60
+        }
+}
---
+++ disc-ammo/sql/disc-ammo.sql
@@ -0,0 +1,2 @@
+INSERT INTO essentialmode.items (name, label, `limit`, rare, can_remove) VALUES ('disc_ammo_pistol', 'Pistol Ammo', 10, 0, 1);
+INSERT INTO essentialmode.items (name, label, `limit`, rare, can_remove) VALUES ('disc_ammo_pistol_large', 'Pistol Ammo Large', 10, 0, 1);
