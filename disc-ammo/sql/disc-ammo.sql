dfad88bdc7836fdf2b7709cbe0e14ddc9a5eef25
rex2630 <rex2630@gmail.com>
2019-12-07 22:39:17 +0100

    Fix SQL

--- disc-ammo/sql/disc-ammo.sql
+++ disc-ammo/sql/disc-ammo.sql
@@ -1,13 +1,11 @@
 create table disc_ammo
 (
-    id bigint unsigned auto_increment,
+    id bigint unsigned auto_increment PRIMARY KEY,
     owner text not null,
     hash text not null,
     count int default 0 not null,
     constraint id
         unique (id)
-
-    PRIMARY KEY (`id`)
 );
 
 
