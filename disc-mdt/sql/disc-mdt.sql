8b4fe104f981d5fc88357c30a94472d5eba8a802
rex2630 <rex2630@gmail.com>
2019-12-08 19:11:08 +0100

    Added sql

---
+++ disc-mdt/sql/disc-mdt.sql
@@ -0,0 +1,20 @@
+create table disc_mdt_crimes
+(
+    id bigint unsigned auto_increment PRIMARY KEY,
+    name text not null,
+    fine int default 0 not null,
+    jailtime int default 0 not null,
+    type text not null,
+    description text not null,
+    constraint id
+        unique (id)
+);
+
+alter table users
+    add darkmode BOOLEAN null;
+
+alter table users
+    add userimage BOOLEAN null;
+
+alter table OWNED_VEHICLES
+    add vehicleimage BOOLEAN null;
